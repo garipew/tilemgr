24551edf64fe60963a988ae125cca931e1aedfea
Gabriel Sá <gabriel10043@live.com>
2025-12-18 09:51:46 -0300

    add Frames class to aid on image slicing

---
+++ Frame.cs
@@ -0,0 +1,16 @@
+namespace Tilemgr;
+
+public class Frame
+{
+	int x;
+	int y;
+	int wid;
+	int hei;
+	public Frame(int x, int y, int wid, int hei)
+	{
+		this.x = x;
+		this.y = y;
+		this.wid = wid;
+		this.hei = hei;
+	}
+}
