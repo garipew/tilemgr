7cbf6cdb9afb5f87e82e92fea6fd51da28ed3b7a
Gabriel Sá <gabriel10043@live.com>
2025-12-23 22:10:51 -0300

    implement ILoadable interface of Project

--- Tilemgr/Project.cs
+++ Tilemgr/Project.cs
@@ -8,40 +8,82 @@ public class Project : ILoadable<Project>
 	private Canvas canvas;
 	private Palette? palette;
 
-	public static Project? Pull(string hash)
+	public static Project? Load(Context c)
+	{
+		using var conn = new SqliteConnection("Data Source=data.db");
+		conn.Open();
+
+		using var cmd = conn.CreateCommand();
+		cmd.CommandText = "SELECT c_path,p_path,t_wid,t_hei FROM projects WHERE hash = $lookup";
+		cmd.Parameters.AddWithValue("$lookup", c.lookup);
+
+		Project? p = null;
+		try
+		{
+			using var reader = cmd.ExecuteReader();
+			if(!reader.Read() || reader.IsDBNull(0) || reader.IsDBNull(1) || reader.IsDBNull(2) || reader.IsDBNull(3))
+			{
+				return null;
+			}
+
+			var canvas = Canvas.Load(new Context(reader.GetString(0)));
+			var palette = Palette.Load(new Context(reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3)));
+			if(canvas == null)
+			{
+				return null;
+			}
+			p = new Project(canvas, palette);
+		} catch(Exception e){
+			System.Console.WriteLine(e.ToString());
+		}
+		return p;
+	}
+
+	public static List<Project> Load()
 	{
 		using var conn = new SqliteConnection("Data Source=data.db");
 		conn.Open();
 		
 		using var cmd = conn.CreateCommand();
-		// TODO(garipew): Writing the query requires to actually
-		// know the layout of the table, I have to figure it out. After
-		// that, it is as simple as:
-		// cmd.CommandText = "SELECT cols FROM table WHERE hash = $hash";
-		// cmd.Parameters.AddWithValue("$hash", hash);
+		cmd.CommandText = "SELECT c_path,p_path,t_wid,t_hei FROM projects";
 
-		// using var reader = cmd.ExecuteReader();
-		// if(!reader.Read() || reader.IsDBNull(0) || reader.IsDBNull(n))
-		// {
-		// 	return null;
-		// }
+		List<Project> projects = new();
+		try
+		{
+			using var reader = cmd.ExecuteReader();
+			while(reader.Read())
+			{
+				if(reader.IsDBNull(0) || reader.IsDBNull(1) || reader.IsDBNull(2) || reader.IsDBNull(3))
+				{
+					continue;
+				}
 
-		return null;
+				var canvas = Canvas.Load(new Context(reader.GetString(0)));
+				var palette = Palette.Load(new Context(reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3)));
+				if(canvas == null)
+				{
+					continue;
+				}
+				projects.Add(new Project(canvas, palette));
+			}
+		} catch(Exception e){
+			System.Console.WriteLine(e.ToString());
+		}
+		return projects;
 	}
 
-	public static void Pull()
-	{
-		// TODO(garipew): Return every single project metadata (not all
-		// metadata, tilesheet is not needed here) stored on DB.
-	}
-
-	public static void Push(Project p)
+	public static void Save(Project p)
 	{
 		// TODO(garipew): This UPDATES a DB entry, this does NOT create
 		// an entry.
 		// This is where the hashing happens, every imutable property
 		// of Project should be used (perhaps include more imutables
 		// like 'CreationDate' and whatnot).
+	}
+
+	public Project(Canvas c, Palette? p){
+		this.canvas = c;
+		this.palette = p;
 	}
 
 	public Project(int wid, int hei){
