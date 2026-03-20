da3138909b3d18616e554503b0f3ce9ee73ece88
Gabriel Sá <gabriel10043@live.com>
2026-03-20 18:53:19 -0300

    fix a possible missing drawing
    race condition on the fallthrough of stage 1, add drawings on atlas load

--- wwwroot/js/editor.js
+++ wwwroot/js/editor.js
@@ -1,84 +1,3 @@
-const Connection = (function () {
-	const ws = new WebSocket(location.pathname + "ws");
-	ws.addEventListener("message", (event) => {
-		Parser.parse(event.data);
-	})
-
-	function send_update(e) {
-		const rect = viewport.getBoundingClientRect();
-		mouse.x = Math.floor((e.clientX - rect.left) / tilemap_canvas.view_wid);
-		mouse.y = Math.floor((e.clientY - rect.top) / tilemap_canvas.view_hei);
-		if(!erase) {
-			ws.send(JSON.stringify({x: mouse.x, y: mouse.y, tile: tile_selected}));
-		} else {
-			ws.send(JSON.stringify({x: mouse.x, y: mouse.y, tile: 0}));
-		}
-	}
-
-	return {
-		send_update
-	};
-})();
-
-const Parser = (function () {
-	let stage = 0;
-
-	function decompress(compressed) {
-		let prologue = compressed.indexOf("\n".charCodeAt(0));
-		prologue++;
-		const header = new TextDecoder("latin1").decode(compressed.slice(0, prologue));
-		const [width,height] = header.split(" ").map(Number);
-		const decompressed = Array.from({ length: height }, () => new Uint8Array(width));
-		let repeats = 0;
-		let element = 0;
-		let wid = 0;
-		let hei = 0;
-		for (let i = prologue; i + 1 < compressed.length; i += 2) {
-			repeats = compressed[i];
-			element = compressed[i + 1];
-			for (let j = 0; j < repeats; j++) {
-				decompressed[hei][wid] = element;
-				wid += 1;
-				if (wid >= width) {
-					wid = 0;
-					hei += 1;
-				}
-				if (hei >= height) {
-					return decompressed;
-				}
-			}
-		}
-		return decompressed;
-	}
-
-	function parse(msg) {
-		var update = JSON.parse(msg)
-		switch(stage){
-		case 0:
-			let compressed = new Uint8Array(update.compressed);
-			tilemap_canvas.restore(update, decompress(compressed));
-			stage++;
-			break
-		case 1:
-			Canvas.load_atlas(update);
-			palette_canvas.gen_selectormap(Canvas.frames.length);
-			stage++;
-			// NOTE(garipew): Intentionally missing break here, to flow
-			// to next stage without waiting for update
-		default:
-			tilemap_canvas.update(update.x, update.y, update.tile);
-			tilemap_canvas.clear();
-			tilemap_canvas.draw();
-			palette_canvas.clear();
-			palette_canvas.draw();
-		}
-	}
-
-	return {
-		parse
-	};
-})();
-
 /*
  * TODO(garipew): Add viewport
  * 			- viewport should contain: cols, rows, x and y
@@ -190,10 +109,94 @@ class Canvas {
 	}
 }
 
-const FPS = 60
-
 const tilemap_canvas = new Canvas(viewport.getContext("2d"));
 const palette_canvas = new Canvas(palette.getContext("2d"));
+
+const Parser = (function () {
+	let stage = 0;
+
+	function decompress(compressed) {
+		let prologue = compressed.indexOf("\n".charCodeAt(0));
+		prologue++;
+		const header = new TextDecoder("latin1").decode(compressed.slice(0, prologue));
+		const [width,height] = header.split(" ").map(Number);
+		const decompressed = Array.from({ length: height }, () => new Uint8Array(width));
+		let repeats = 0;
+		let element = 0;
+		let wid = 0;
+		let hei = 0;
+		for (let i = prologue; i + 1 < compressed.length; i += 2) {
+			repeats = compressed[i];
+			element = compressed[i + 1];
+			for (let j = 0; j < repeats; j++) {
+				decompressed[hei][wid] = element;
+				wid += 1;
+				if (wid >= width) {
+					wid = 0;
+					hei += 1;
+				}
+				if (hei >= height) {
+					return decompressed;
+				}
+			}
+		}
+		return decompressed;
+	}
+
+	function parse(msg) {
+		var update = JSON.parse(msg)
+		switch(stage){
+		case 0:
+			let compressed = new Uint8Array(update.compressed);
+			tilemap_canvas.restore(update, decompress(compressed));
+			stage++;
+			break
+		case 1:
+			Canvas.load_atlas(update);
+			Canvas.atlas.onload = () => {
+				palette_canvas.gen_selectormap(Canvas.frames.length);
+				tilemap_canvas.clear();
+				tilemap_canvas.draw();
+				palette_canvas.clear();
+				palette_canvas.draw();
+			}
+			stage++;
+			break;
+		default:
+			tilemap_canvas.update(update.x, update.y, update.tile);
+			tilemap_canvas.clear();
+			tilemap_canvas.draw();
+			palette_canvas.clear();
+			palette_canvas.draw();
+		}
+	}
+
+	return {
+		parse
+	};
+})();
+
+const Connection = (function () {
+	const ws = new WebSocket(location.pathname + "ws");
+	ws.addEventListener("message", (event) => {
+		Parser.parse(event.data);
+	})
+
+	function send_update(e) {
+		const rect = viewport.getBoundingClientRect();
+		mouse.x = Math.floor((e.clientX - rect.left) / tilemap_canvas.view_wid);
+		mouse.y = Math.floor((e.clientY - rect.top) / tilemap_canvas.view_hei);
+		if(!erase) {
+			ws.send(JSON.stringify({x: mouse.x, y: mouse.y, tile: tile_selected}));
+		} else {
+			ws.send(JSON.stringify({x: mouse.x, y: mouse.y, tile: 0}));
+		}
+	}
+
+	return {
+		send_update
+	};
+})();
 
 let mouse = {x: 0, y: 0, down: false}
 let tile_selected = 1
