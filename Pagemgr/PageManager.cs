3657ce3348bb96017c19bc6f5a091b638c7c23a7
Gabriel Sa <gabriel10043@live.com>
2026-05-02 18:57:48 -0300

    centralizes DB interactions
    This commit centralizes the majority of the DB interactions, now handled
    by EF Core, to the PageManager Class. Two new methods are created,
    PageManager.Connect and PageManager.Disconnect, which ensure that
    Projects are unloaded of memory when last connection closes.

--- Pagemgr/PageManager.cs
+++ Pagemgr/PageManager.cs
@@ -1,20 +1,64 @@
 using System.Net.WebSockets;
 using System.Collections.Concurrent;
+using Microsoft.EntityFrameworkCore;
+
 using Tilemgr;
+using Data;
 
 namespace Pagemgr;
 
-public class PageManager<T> where T : ILoadable<T>
+public class PageManager
 {
-	private readonly ConcurrentDictionary<string, Page<T>> _pages = new();
+	private IDbContextFactory<ProjectContext> _contextFactory;
 
-	public bool TryGet(string key, out Page<T>? p) => _pages.TryGetValue(key, out p);
-	public bool TryRemove(string key, out Page<T>? p) => _pages.TryRemove(key, out p);
+	private readonly ConcurrentDictionary<string, Page> _pages = new();
 
-	public Page<T> GetOrCreate(string hash)
-	{
-		T? obj = T.Load(new Context(hash));
-		return _pages.GetOrAdd(hash, new Page<T>(obj));
+	public bool TryGet(string key, out Page? p) => _pages.TryGetValue(key, out p);
+	public bool TryRemove(string key, out Page? p) => _pages.TryRemove(key, out p);
+	public bool TryAdd(string key, Page p) => _pages.TryAdd(key, p);
+
+	public IEnumerable<Page> Pages => _pages.Values;
+
+	public PageManager(IDbContextFactory<ProjectContext> factory) {
+		_contextFactory = factory;
 	}
 
+	public Page GetOrCreate(string hash)
+	{
+		Page? p;
+		if(_pages.TryGetValue(hash, out p) && p != null) {
+			return p;
+		}
+		using var context = _contextFactory.CreateDbContext();
+		Project? proj = context.Projects.Where(p => p.Hash == hash).FirstOrDefault();
+		if(proj != null) {
+			proj.canvas.DrawableLayer = Canvas.decompress(proj.canvas.Compressed);
+		}
+		return _pages.GetOrAdd(hash, new Page(proj, hash));
+	}
+
+	public bool Connect(Page p, Client c)
+	{
+		return p.Connect(c);
+	}
+
+	public bool Disconnect(Page p, Client c)
+	{
+		Page? removed;
+
+		if(p.Disconnect(c) && p.IsEmpty && p.Data != null) {
+			p.Data.canvas.Compressed = p.Data.canvas.compress();
+			using var context = _contextFactory.CreateDbContext();
+			var proj = context.Projects.Where(pr => pr.Hash == p.Data.Hash).FirstOrDefault();
+			if(proj == null) {
+				context.Projects.Add(p.Data);
+			} else {
+				proj.canvas = p.Data.canvas;
+				proj.palette = p.Data.palette;
+			}
+			context.SaveChanges();
+			return _pages.TryRemove(p.Hash, out removed);
+		}
+		return false;
+	}
 }
